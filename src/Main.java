911a1863b22fc6e075857b247c587ad03abd208f
See <shannkevin.see@g.msuiit.edu.ph>
2022-04-28 10:51:36 +0800

    main file

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,9 @@ public class Main {
 
         Teacher t1 = new Teacher();
         Student s1 = new Student();
-<<<<<<< HEAD
+
         System.out.println(t1.y);
-=======
-        System.out.println(t1.x);
->>>>>>> main file
+
         System.out.println(s1.x);
     }
 }
