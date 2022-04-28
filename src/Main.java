4e7c574867f5c19851e1b4f05d35a5d1b6ea2e03
MagnusR3d <104538534+MagnusR3d@users.noreply.github.com>
2022-04-28 10:22:57 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args){
 
         Teacher t1 = new Teacher();
+        Student s1 = new Student();
         System.out.println(t1.x);
+        System.out.println(s1.x);
     }
 }
