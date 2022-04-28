6990e698a83e5efaad41bec65d0c5cc64a54bb6c
MagnusR3d <104538534+MagnusR3d@users.noreply.github.com>
2022-04-28 10:48:00 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
         Teacher t1 = new Teacher();
         Student s1 = new Student();
-        System.out.println(t1.x);
+        System.out.println(t1.y);
         System.out.println(s1.x);
     }
 }
